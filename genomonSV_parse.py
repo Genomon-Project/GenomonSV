777eb19e7fd5ea158a3459c3f3a26badc6bcb00b
Shiraishi_Yuichi <yshira@sc095.(none)>
2015-06-24 18:02:26 +0900

    modifiy genomonSV_parse.py

--- genomonSV_parse.py
+++ genomonSV_parse.py
@@ -6,6 +6,7 @@
 
 """
 
+import sys
 import argparse
 import yaml
 
@@ -14,26 +15,29 @@ def genomonSV_parse():
     ####################
     # parse arguments
     parser = argparse.ArgumentParser(description = "Parse bam file for breakpoint and improper read pairs")
-    parser.add_argument("sampleInfoFile", metavar = "sample.yaml", type = str, nargs = 1,
+    parser.add_argument("sampleInfoFile", metavar = "sample.yaml", type=argparse.FileType('r'), 
                         help = "input sample information file (yaml format)")
-    parser.add_argument("paramInfoFile", metavar = "param.yaml", type = str, nargs = 1,
+    parser.add_argument("paramInfoFile", metavar = "param.yaml", type=argparse.FileType('r'),
                         help = "parameter information file (yaml format)")
     args = parser.parse_args()
     ####################
 
-
     ####################
-    # load yaml files
+    # load config files
     try:
-        sampleConf = yaml.load(file(args.samleInfoFile, 'r'))
+        sampleConf = yaml.load(args.sampleInfoFile)
     except yaml.YAMLError, exc:
         print "Error in sample information file:", exc
 
     try:
-        paramConf = yaml.load(file(args.samleInfoFile, 'r'))
+        paramConf = yaml.load(args.paramInfoFile)
     except yaml.YAMLError, exc:
-        print "Error in parameter information file:", exc
+        print "Error in sample information file:", exc
+
+    # checking the config files (hopefully, create conf.py to check each key
     ####################
+
+
 
     # check the existence of input files
 
@@ -41,19 +45,19 @@ def genomonSV_parse():
 
 
     # parse breakpoint containing read pairs from input bam files
-    parseJunctionFromBam()
+    # parseJunctionFromBam()
 
     # parse improper read pairs from input bam files
-    parseImproperFromBam()
+    # parseImproperFromBam()
 
     # sort and compress the parsed breakpoint containing read pairs
-    sortJunction()
+    # sortJunction()
 
     # merge and cluster parsed improper read pairs from input bam files
-    clusterImproper()
+    # clusterImproper()
 
     # merge and cluster parsed breakpoint containing read pairs
-    clusterJunction()
+    # clusterJunction()
 
 
 
