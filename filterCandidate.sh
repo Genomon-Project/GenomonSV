802a3800a950eb21324227e5851010b8c145fc5a
friend1ws <friend1ws@gmail.com>
2015-06-27 10:52:39 +0900

    move genomonSV_parse.sh and its subscripts to sh directory

--- filterCandidate.sh
+++ filterCandidate.sh
@@ -10,20 +10,20 @@ TUMORDIR=$1
 CONTROL=$2
 MATCHEDNORMAL=$3
 
-echo "python filterLengthNum.py ${TUMORDIR}/merge.junction.summarized.bedpe.gz 3 10 > ${TUMORDIR}/merge.junction.summarized.filt1.bedpe"
-python filterLengthNum.py ${TUMORDIR}/merge.junction.summarized.bedpe.gz 3 10 > ${TUMORDIR}/merge.junction.summarized.filt1.bedpe
+echo "python filterLengthNum.py ${TUMORDIR}/merge.junction.summarized.bedpe.gz 1 10 > ${TUMORDIR}/merge.junction.summarized.filt1.bedpe"
+python filterLengthNum.py ${TUMORDIR}/merge.junction.summarized.bedpe.gz 1 10 > ${TUMORDIR}/merge.junction.summarized.filt1.bedpe
 check_error $?
 
-echo "python filterNonMatchControl.py ${TUMORDIR}/merge.junction.summarized.filt1.bedpe ${CONTROL} ${MATCHEDNORMAL} 3 > ${TUMORDIR}/merge.junction.summarized.filt2.bedpe"
-python filterNonMatchControl.py ${TUMORDIR}/merge.junction.summarized.filt1.bedpe ${CONTROL} ${MATCHEDNORMAL} 3 > ${TUMORDIR}/merge.junction.summarized.filt2.bedpe 
+echo "python filterNonMatchControl.py ${TUMORDIR}/merge.junction.summarized.filt1.bedpe ${CONTROL} ${MATCHEDNORMAL} 1 > ${TUMORDIR}/merge.junction.summarized.filt2.bedpe"
+python filterNonMatchControl.py ${TUMORDIR}/merge.junction.summarized.filt1.bedpe ${CONTROL} ${MATCHEDNORMAL} 1 > ${TUMORDIR}/merge.junction.summarized.filt2.bedpe 
 check_error $?
 
 echo "python addImproperInfo.py ${TUMORDIR}/merge.junction.summarized.filt2.bedpe ${TUMORDIR}/merge.improperPair.summarized.bedpe.gz > ${TUMORDIR}/merge.junction.summarized.filt3.bedpe"
 python addImproperInfo.py ${TUMORDIR}/merge.junction.summarized.filt2.bedpe ${TUMORDIR}/merge.improperPair.summarized.bedpe.gz > ${TUMORDIR}/merge.junction.summarized.filt3.bedpe 
 check_error $?
 
-echo "python filterMergedJunc.py ${TUMORDIR}/merge.junction.summarized.filt3.bedpe 3 40 75 > ${TUMORDIR}/merge.junction.summarized.filt4.bedpe"
-python filterMergedJunc.py ${TUMORDIR}/merge.junction.summarized.filt3.bedpe 3 40 75 > ${TUMORDIR}/merge.junction.summarized.filt4.bedpe 
+echo "python filterMergedJunc.py ${TUMORDIR}/merge.junction.summarized.filt3.bedpe 3 40 120 > ${TUMORDIR}/merge.junction.summarized.filt4.bedpe"
+python filterMergedJunc.py ${TUMORDIR}/merge.junction.summarized.filt3.bedpe 3 40 120 > ${TUMORDIR}/merge.junction.summarized.filt4.bedpe 
 check_error $?
 
 echo "python removeClose.py ${TUMORDIR}/merge.junction.summarized.filt4.bedpe > ${TUMORDIR}/merge.junction.summarized.filt5.bedpe"
