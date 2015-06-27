05a7d46e76c34601aa410d0d6ec40bcb8828a083
friend1ws <friend1ws@gmail.com>
2015-06-27 11:11:05 +0900

    create filterJuncNumAndSize function (1st step) in filterFunction.py

---
+++ filterFunction.py
@@ -0,0 +1,47 @@
+#! /usr/local/bin/python
+
+"""
+     functions for filtering candidates of structural variations
+"""
+
+import gzip
+
+
+def filterJuncNumAndSize(inputFilePath, outputFilePath, Params):
+     
+    """
+        script for filtering by the length of SV size and support read junctions
+    """
+
+    junc_num_thres = Params["junc_num_thres"]
+    SV_size_thres = Params["SV_size_thres"]
+
+    hIN = gzip.open(inputFilePath, 'r')
+    hOUT = open(outputFilePath, 'w')
+
+    for line in hIN:
+        F = line.rstrip('\n').split('\t')
+
+        ##########
+        # for now only consider long ranged SV??
+        svLen = abs(int(F[2]) - int(F[5]))
+        if F[7] != "---": svLen = svLen + len(F[7])
+        if F[0] == F[3] and svLen < int(SV_size_thres): continue
+        ########## 
+    
+        ##########
+        # enumerate support read number
+        IDs = F[6].split(';')
+        juncSupport = int(len(IDs))
+
+        # skip if the number of suppor read is below the minSupReadNum 
+        if juncSupport < int(junc_num_thres): continue
+        ##########
+
+        print >> hOUT, '\t'.join(F)
+
+
+    hIN.close()
+    hOUT.close()
+
+
---
+++ genomonSV_filt.py
@@ -0,0 +1,54 @@
+#! /usr/local/bin/python
+
+"""
+
+    Genomon SV filt: extracting candidates of structural variations from clustered breakpoint-containing and improperly aligned read pairs
+
+"""
+
+import sys
+import argparse
+import config 
+import utils
+import parseFunction
+import filterFunction 
+
+def genomonSV_parse():
+
+    ####################
+    # parse arguments
+    parser = argparse.ArgumentParser(description = "Parse bam file for breakpoint and improper read pairs")
+    parser.add_argument("sampleInfoFile", metavar = "sample.yaml", type = str,
+                        help = "input sample information file (yaml format)")
+    parser.add_argument("paramInfoFile", metavar = "param.yaml", type = str,
+                        help = "parameter information file (yaml format)")
+    args = parser.parse_args()
+    ####################
+
+
+    ####################
+    # load config files
+    global sampleConf
+    sampleConf = config.sample_yaml_config_parse(args.sampleInfoFile)
+
+    global paramConf
+    paramConf = config.param_yaml_contig_parse(args.paramInfoFile)
+    ####################
+
+
+    ####################
+    # file existence check
+
+ 
+    ####################
+    outputPrefix = sampleConf["output_dir"] + "/" + sampleConf["label"]
+
+    filterFunction.filterJuncNumAndSize(outputPrefix + ".junction.clustered.bedpe.gz",
+                                        outputPrefix + ".junction.clustered.filt1.bedpe",
+                                        paramConf["filterCondition"])
+
+    ####################
+
+
+if __name__ == "__main__":
+    genomonSV_parse()
