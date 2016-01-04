fadff18c326b2ced58fe89443c337ff884841e28
friend1ws <friend1ws@gmail.com>
2016-01-04 15:06:03 +0900

    version change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='genomonsv',
-      version='0.1.0',
+      version='0.1.1',
       description='Python tools for detecting somatic structural variation from cancer genome sequencing data.',
       author='Yuichi Shiraishi',
       author_email='friend1ws@gamil.com',
