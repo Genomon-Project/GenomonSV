9d1a8a3c3cfda183b1ef3daed5d78a67ada18d8f
friend1ws <friend1ws@gmail.com>
2018-01-23 12:43:58 +0900

    modified version

--- genomon_sv/arg_parser.py
+++ genomon_sv/arg_parser.py
@@ -9,7 +9,7 @@ def create_parser():
     # top level parser
     parser = argparse.ArgumentParser(prog = "GenomonSV", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("--version", action = "version", version = "GenomonSV-0.5.0")
+    parser.add_argument("--version", action = "version", version = "GenomonSV-0.5.1a")
 
     subparsers = parser.add_subparsers()
 
