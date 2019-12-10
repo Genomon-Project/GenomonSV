e1ae3a6e0a3cca05dab6e35c179c010a1da8f750
friend1ws <friend1ws@gmail.com>
2019-12-10 16:49:48 +0000

    modify version consistency

--- genomon_sv/arg_parser.py
+++ genomon_sv/arg_parser.py
@@ -9,7 +9,7 @@ def create_parser():
     # top level parser
     parser = argparse.ArgumentParser(prog = "GenomonSV", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("--version", action = "version", version = "GenomonSV-0.7.1")
+    parser.add_argument("--version", action = "version", version = "GenomonSV-0.7.2")
 
     subparsers = parser.add_subparsers()
 
