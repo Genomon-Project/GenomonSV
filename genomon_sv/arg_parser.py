192bd974235fb8b9faddcdf3ed908e7457ee5510
Ai Okada <aokada@ncc.go.jp>
2022-11-30 17:46:36 +0900

    Update arg_parser.py

--- genomon_sv/arg_parser.py
+++ genomon_sv/arg_parser.py
@@ -9,7 +9,7 @@ def create_parser():
     # top level parser
     parser = argparse.ArgumentParser(prog = "GenomonSV", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("--version", action = "version", version = "GenomonSV-0.8.2")
+    parser.add_argument("--version", action = "version", version = "GenomonSV-0.8.3")
 
     subparsers = parser.add_subparsers()
 
